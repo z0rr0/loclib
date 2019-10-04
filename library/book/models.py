67d45ff97c81d91d875d246170a01ce62df38f85
Alexander Zaytsev <me@axv.email>
2019-10-04 15:59:56 +0300

    models updated

--- library/book/models.py
+++ library/book/models.py
@@ -1,21 +1,7 @@
-import json
+import hashlib
 
 from django.db import models
 from django.utils.translation import gettext_lazy as _
-
-
-class JSONField(models.TextField):
-    description = _("JSON")
-
-    def get_internal_type(self):
-        return "JSONField"
-
-    def from_db_value(self, value, expression, connection):
-        return json.loads(value) if value else value
-
-    def to_python(self, value):
-        value = super().to_python(value)
-        return json.dumps(value) if value else value
 
 
 class CreatedUpdatedModel(models.Model):
@@ -28,7 +14,7 @@ class CreatedUpdatedModel(models.Model):
 
 class NamedModel(CreatedUpdatedModel):
     name = models.CharField(_('name'), max_length=1024)
-    url = models.URLField(_('url'), max_length=4096, blank=True, null=True)
+    homepage = models.URLField(_('homepage'), max_length=4096, blank=True, null=True)
     comment = models.TextField(_('comment'), blank=True)
 
     class Meta:
@@ -73,14 +59,14 @@ class Book(CreatedUpdatedModel):
     )
 
     title = models.CharField(_('title'), max_length=4096)
-    path = models.FilePathField(_('path'), max_length=4096, unique=True)
+    path = models.FilePathField(_('path'), max_length=8192, unique=True)
     language = models.CharField(_('language'), max_length=32, choices=LANGUAGE_CHOICES, default=LANG_RU)
-    metadata = JSONField(_('metadata'), blank=True)
+    metadata = models.TextField(_('metadata'), blank=True)
     isbn10 = models.CharField(_('ISBN-10'), max_length=32, blank=True)
     isbn13 = models.CharField(_('ISBN-13'), max_length=64, blank=True)
     publisher = models.ForeignKey(Publisher, null=True, blank=True, on_delete=models.SET_NULL)
-    publication = models.DateField(_('publication'), null=True, blank=True)
-    comment = models.TextField(_('comment'), blank=True)
+    pubdate = models.DateField(_('pubdate'), null=True, blank=True)
+    checksum = models.CharField(_('checksum'), max_length=64, blank=True)
     authors = models.ManyToManyField(Author)
     tags = models.ManyToManyField(Tag)
 
@@ -91,3 +77,17 @@ class Book(CreatedUpdatedModel):
 
     def __str__(self) -> str:
         return self.title
+
+    def get_hash(self) -> str:
+        h = hashlib.sha256()
+        h.update(open(self.path, 'rb').read())
+        return h.hexdigest()
+
+
+class Comment(CreatedUpdatedModel):
+    book = models.ForeignKey(Book, related_name='comments', on_delete=models.CASCADE)
+    message = models.TextField(_('message'))
+
+    class Meta:
+        verbose_name = _('comment')
+        verbose_name_plural = _('comments')
--- library/library/settings.py
+++ library/library/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import sys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -119,3 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+
+if 'test' not in sys.argv:
+    # overwrite custom settings
+    try:
+        from .local_settings import *
+    except ImportError:
+        print('local settings ImportError')
